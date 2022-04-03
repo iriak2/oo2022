73dd8c389596696b34b9d9332a7bb7b2f0a48a66
Kairi Käpa <90192427+iriak2@users.noreply.github.com>
2022-04-03 19:54:13 +0200

    Update hobuTest.java

--- 07_junit/hobuTest.java
+++ 07_junit/hobuTest.java
@@ -7,8 +7,4 @@ public class hobuTest {
     @Test public void varsaKaal(){
         assertEquals(45, hobu2.varsaKaal(450));
     }
-    /*@Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }*/
 }
