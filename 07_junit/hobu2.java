3c443d510aed3f11b335448a230755c407da2b3d
Kairi Käpa <90192427+iriak2@users.noreply.github.com>
2022-04-03 21:14:42 +0200

    Update hobu2.java

--- 07_junit/hobu2.java
+++ 07_junit/hobu2.java
@@ -6,8 +6,14 @@ public class hobu2{
         return ajutine;
     }
 
+    public static double paevaHein(double hobuKaal){
+        double ajutine2 = hobuKaal * 0.02; //hobune peaks tarbima 2% oma kehakaalust iga paev
+        return ajutine2;
+    }
+
     public static void main(String[] args) {
         System.out.println(varsaKaal(450));
+        System.out.println(paevaHein(500.0));
     }
     
 }
